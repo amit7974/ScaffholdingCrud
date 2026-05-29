1609b498134e71b26386b4b2f04535640a66ea74
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2026-05-29 23:36:04 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScallfoldingCrud.Models
+{
+    public class Employee
+    {
+
+        [DisplayName("Employee ID")]
+        public int empid { get; set; }
+
+        [DisplayName("Employee Name")]      
+        public string name { get; set; }
+
+        [DisplayName("Gender")]
+        public string gender { get; set; }
+
+        [DisplayName("Date of Birth")]
+        public DateTime dob { get; set; }
+
+        [DisplayName("Email ID")]
+        public string emailid { get; set; }
+
+    }
+}
